4fb16049fa767f3645b07657ffa8fa7ffb2f3aff
avillegas1717 <134570292+avillegas1717@users.noreply.github.com>
2023-10-05 21:12:29 -0500

    Delete UI layer files without Identity

--- StoreFront.UI.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace StoreFront.UI.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
